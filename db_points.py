187a438b1e71c14173e550fe1e85edef6ff61638
elekozlova <helena.kozlova@tut.by>
2021-10-30 08:11:06 +0300

    change data_url

--- db_points.py
+++ db_points.py
@@ -5,21 +5,24 @@ from typing import List
 from typing import Optional
 import psycopg2
 from dotenv import load_dotenv, find_dotenv
+import urllib.parse as urlparse
 
 
 load_dotenv(find_dotenv())
 
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+dbname = url.path[1:]
+host = url.hostname
+password = url.password
+user = url.username
+#port = url.port
+dsn = f"{user=} {password=} {host=} {dbname=}"
+print (dsn)
+
+
 
 def execute_sql(sql: str) -> List[tuple]:
-    dbname = os.getenv("DATABASE_NAME")
-    host = os.getenv("DATABASE_HOST")
-    password = os.getenv("DATABASE_PASSWORD")
-    user = os.getenv("DATABASE_USER")
-    dsn = f"{user=} {password=} {host=} {dbname=}"
-
     rows = []
-
-
     with closing(psycopg2.connect(dsn)) as connection:
         with closing(connection.cursor()) as cursor:
             cursor.execute(sql)
