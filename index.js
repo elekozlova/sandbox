2c62139f49021794cc89fa14a69589f6d6b44713
Alexander Sidorov <alexander@sidorov.dev>
2021-11-29 03:55:40 +0300

    implemented swap

--- index.js
+++ index.js
@@ -83,8 +83,13 @@ async function doPopulateUI(forced = false) {
         if (index === 0) {
             let hFrom = document.createElement("h3");
             hFrom.innerText = `🌇 ${vector.from_.name}`;
+            hFrom.id = `id-city-${vector.from_.id}`;
             hFrom.cityId = `${vector.from_.id}`;
-            routeContainer.appendChild(hFrom);
+            hFrom.style.display = "inline-block";
+            let hFromDiv = document.createElement("div");
+            hFromDiv.appendChild(hFrom);
+            await doSetUpControls(hFromDiv);
+            routeContainer.appendChild(hFromDiv);
         }
 
         let pPath = document.createElement("p");
@@ -95,9 +100,97 @@ async function doPopulateUI(forced = false) {
 
         let hTo = document.createElement("h3");
         hTo.innerText = `🏙 ${vector.to.name}`;
+        hTo.id = `id-city-${vector.to.id}`;
         hTo.cityId = `${vector.to.id}`;
-        routeContainer.appendChild(hTo);
+        hTo.style.display = "inline-block";
+        let hToDiv = document.createElement("div");
+        hToDiv.appendChild(hTo);
+        await doSetUpControls(hToDiv);
+        routeContainer.appendChild(hToDiv);
     }
+}
+
+
+async function doSetUpControls(cityHeadingDiv) {
+    let heading = cityHeadingDiv.getElementsByTagName("H3")[0];
+
+    let div = document.createElement("div");
+    div.style.margin = div.style.padding = "0";
+    div.style.display = "inline-block";
+    div.style.width = "60px";
+
+    let btnUp = document.createElement("button");
+    btnUp.innerText = "🔼";
+    btnUp.className = "control-button";
+    btnUp.addEventListener("click", async function (event) {
+        await doMoveCityUp(heading.cityId);
+    });
+    div.appendChild(btnUp);
+
+    let btnDown = document.createElement("button");
+    btnDown.innerText = "🔽";
+    btnDown.className = "control-button";
+    btnDown.addEventListener("click", async function (event) {
+        await doMoveCityDown(heading.cityId);
+    });
+    div.appendChild(btnDown);
+
+    cityHeadingDiv.appendChild(div);
+}
+
+
+async function doMoveCityUp(city) {
+    city = parseInt(city);
+    const cities = await getCitiesIds();
+    const index = cities.findIndex((elm) => {
+        return elm === city;
+    });
+    if (index === -1) {
+        console.error(`invalid city ${city} not found in ${cities}`);
+        return;
+    }
+    const newIndex = (index === 0) ? cities.length - 1 : index - 1;
+    const newCity = cities[newIndex];
+    await doSwap(city, newCity);
+}
+
+
+async function doMoveCityDown(city) {
+    city = parseInt(city);
+    const cities = await getCitiesIds();
+    const index = cities.findIndex((elm) => {
+        return elm === city;
+    });
+    if (index === -1) {
+        console.error(`invalid city ${city} not found in ${cities}`);
+        return;
+    }
+    const newIndex = (index === (cities.length - 1)) ? 0 : index + 1;
+    const newCity = cities[newIndex];
+    await doSwap(city, newCity);
+}
+
+
+async function doSwap(city0, city1) {
+    await apiSwap(city0, city1);
+    await doPopulateRouteLocalStorage();
+    await doPopulateUI(true);
+    await doRefreshMap(true);
+}
+
+
+async function apiSwap(id1, id2) {
+    return apiCall("/swap", {json: {id1: id1, id2: id2}, method: "POST"});
+}
+
+
+async function getCitiesIds() {
+    let route = JSON.parse(localStorage.getItem("route"));
+    let cities = [route.path[0].from_.id];
+    for (const vector of route.path) {
+        cities.push(vector.to.id);
+    }
+    return cities;
 }
 
 
